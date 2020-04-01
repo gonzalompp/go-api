3eb49417266a4a0f08aed6bb5786a56325e5b8c6
Gonzalo Perez <gonzalompp@gmail.com>
2020-04-01 19:51:51 +0200

    add env variable for hello message format

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -10,6 +11,14 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+// Env var key for the message content
+const EnvHelloMessage = "SECRET_HELLO_MESSAGE"
+
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hola, %s!", r.URL.Path[1:])
+	// values
+	name := r.URL.Path[1:]
+	message := os.Getenv(EnvHelloMessage)
+
+	// response
+	fmt.Fprintf(w, message, name)
 }
